517d168cdc9e39919846ed82dade32ad11e9d9a9
ASherry114 <andrew.sherry@chironix.com>
2026-02-11 13:31:42 +0800

    removed old arg parser

--- field_utils/cli_helpers.py
+++ field_utils/cli_helpers.py
@@ -1,36 +1,6 @@
 import argparse
 
 
-def mission_info_parser() -> argparse.ArgumentParser:
-    """
-    Parse the mission generation arguments.
-    :return: Argument parser.
-    """
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument(
-        "--task_list",
-        "-l",
-        type=str,
-        default="tasks.yaml",
-        help="List of tasks to create a mission, order matters.",
-    )
-    parser.add_argument(
-        "--name",
-        "-n",
-        type=str,
-        default="default_mission.yaml",
-        help="Name of the mission file. Will be included in the mission file and used as export name.",
-    )
-    parser.add_argument(
-        "--environment",
-        "-e",
-        type=str,
-        default="environment.yaml",
-        help="Name of the related environment file.",
-    )
-    return parser
-
-
 def env_info_parser() -> argparse.ArgumentParser:
     """
     Parse the mission generation arguments.
